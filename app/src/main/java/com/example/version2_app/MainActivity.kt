5877193d0f520a4de09b775cf1149d0ae439db21
Roxxiebstar <kathebuitragor8@gmail.com>
2025-04-10 17:05:16 -0500

    En MainActivity.kt Se pudo hacer todo correctamente para que el usuario se guardara en la tabla una vez pulsará click en el buttonSave. Ahora siguen las pruebas para ver que tan funcional quedó.

--- app/src/main/java/com/example/version2_app/MainActivity.kt
+++ app/src/main/java/com/example/version2_app/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.example.version2_app
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
+    // Referencia al DAO
+    private lateinit var userDao: UserDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //Referenciar a los elementos de mi IU principales
+        val editTextName = findViewById<EditText>(R.id.editNombre)
+        val buttonSave = findViewById<Button>(R.id.buttonSave)
+
+        // Obtener instancia de la base de datos y el DAO
+        val db = AppDatabase.getDatabase(applicationContext)
+        userDao = db.userDao()
+
+        // Configurar el botón para guardar el usuario
+        buttonSave.setOnClickListener {
+            val name = editTextName.text.toString()
+
+            if (name.isNotBlank()) {
+                // Llamar al insertUser desde una corrutina
+                CoroutineScope(Dispatchers.IO).launch {
+                    val user = User(id = 1, name = name)
+                    userDao.insertUser(user)
+                    // Verificamos que se haya guardado consultándolo
+                    val savedUser = userDao.getUser()
+
+                    if (savedUser != null) {
+                        runOnUiThread {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Usuario guardado: ${savedUser.name}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
+            }
         }
     }
 }
